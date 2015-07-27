b0eede725377069cfeb5c5648e6b690d9a3cb19f
Jade <dongqiaoruby@gmail.com>
2015-07-27 22:23:16 +0800

    image croper example

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,52 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(avatar_params)
+      redirect_to users_path
+    else
+      render :new
+    end
+  end
+
+  def create
+    @user = User.new(user_params)
+    respond_to do |format|
+      if @user.save
+        format.html {
+          if params[:user][:avatar].present?
+            render :crop  ## Render the view for cropping
+          else
+            redirect_to @user, notice: 'User was successfully created.'
+          end
+        }
+        format.json { render action: 'show', status: :created, location: @user }
+      else
+        format.html { render action: 'new' }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:avatar_crop_x, :avatar_crop_y, :avatar_crop_w, :avatar_crop_h, :avatar)
+  end
+  def avatar_params
+    params.require(:user).permit(:avatar_crop_x, :avatar_crop_y, :avatar_crop_w, :avatar_crop_h)
+  end
+end
---
+++ app/models/user.rb
@@ -0,0 +1,4 @@
+class User < ActiveRecord::Base
+  mount_uploader :avatar, AvatarUploader
+  crop_uploaded :avatar  ## Add this
+end
